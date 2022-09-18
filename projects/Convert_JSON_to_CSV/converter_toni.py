591c97355aadbdc8afbc7762e069f26c836a358a
Toni <toni.dezman@gmail.com>
2022-09-18 10:10:40 +0200

    refactor

--- projects/Convert_JSON_to_CSV/converter_toni.py
+++ projects/Convert_JSON_to_CSV/converter_toni.py
@@ -3,20 +3,19 @@ import csv
 
 
 def convert_json_to_csv(json_filename, csv_filename):
-    with open("input.json", "r") as f:
-        lines = f.readlines()
-        people = json.loads("".join(lines))
+    with open(json_filename, "r") as f:
+        json_file = f.read()
+        people = json.loads(json_file)
 
-    with open('output_toni.csv', 'w', newline='') as csvfile:
+    with open(csv_filename, "w") as f:
         fieldnames = people[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
         writer.writeheader()
         for person in people:
             writer.writerow(person)
 
 
-if __name__ == '__main__':
-    json_filename = 'input.json'
-    csv_filename = 'output_toni.csv'
+if __name__ == "__main__":
+    json_filename = "input.json"
+    csv_filename = "output_toni.csv"
     convert_json_to_csv(json_filename, csv_filename)
